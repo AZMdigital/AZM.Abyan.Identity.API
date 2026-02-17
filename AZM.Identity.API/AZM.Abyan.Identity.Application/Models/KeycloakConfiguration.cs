6a99288b8e88602e984b2529529026c5ad3db115
ALQ <ALQ@DESKTOP-O9VTJHC>
2026-02-17 16:15:27 +0200

    d

--- AZM.Identity.API/AZM.Abyan.Identity.Application/Models/KeycloakConfiguration.cs
+++ AZM.Identity.API/AZM.Abyan.Identity.Application/Models/KeycloakConfiguration.cs
@@ -5,6 +5,7 @@ public class KeycloakConfiguration
     public string BaseUrl { get; set; } = string.Empty;
     public string Realm { get; set; } = string.Empty;
     public string ClientId { get; set; } = string.Empty;
+    public string? ClientInternalId { get; set; } // Keycloak client internal UUID (from URL)
     public string? ClientSecret { get; set; }
     public string AdminUsername { get; set; } = string.Empty;
     public string AdminPassword { get; set; } = string.Empty;
